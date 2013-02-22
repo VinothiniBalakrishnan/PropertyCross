e72cbd6a14f37072b62b814c696667be431ace09
vinothini balakrishnan <vinothinibj@gmail.com>
2013-02-22 16:24:56 +0530

    In Favourite model find_all_by_guid method added to get all favourite items

--- app_in_rhodes/app/Favourite/favourite.rb
+++ app_in_rhodes/app/Favourite/favourite.rb
@@ -1,10 +1,6 @@
-# The model has already been created by the framework, and extends Rhom::RhomObject
-# You can add more methods here
 class Favourite
   include Rhom::PropertyBag
-
-  # Uncomment the following line to enable sync with Favourite.
-  # enable :sync
-
-  #add model specific code here
+  def self.find_all_by_guid(guid)
+    find(:all, :conditions => { "guid" => guid })
+  end
 end
